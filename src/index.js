6863bf487853a7500e6cbc1d3e30e0233af8d69b
opheliah95 <opheliah95@gmail.com>
2022-08-10 18:03:29 +0100

    managed to create component with map function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Board extends React.Component {
+
+  render() {
+    const data = [1, 2, 3];
+    console.log(data)
+
+    return (
+      <div className="row-1">
+        {data.map((num) => (
+          <button>{num}</button>
+        ))}
+      </div>
+    );
+  }
+
+}
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const element = <Board />;
+root.render(element);
